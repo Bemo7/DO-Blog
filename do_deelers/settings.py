63950668a42a3a8e94f007aae9b2029d3b044dbf
BEMO <82284405+Jaggzy7@users.noreply.github.com>
2023-04-07 23:44:19 +0100

    Fix... uhhhh

--- do_deelers/settings.py
+++ do_deelers/settings.py
@@ -26,11 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
-ALLOWED_HOSTS = [
-    os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
-    ]
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-DEBUG = str(os.getenv("DEBUG", True)) == "True"
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 # Added for Digital Ocean
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
